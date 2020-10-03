3f486511a72eb1cbfb798f357c5ae1d3313e38a5
Tyler Kalosza <tyler.kalosza@gmail.com>
2020-10-02 21:53:21 -0400

    Added campgrounds page.

--- app.js
+++ app.js
@@ -9,6 +9,16 @@ app.get("/", (req, res) => {
     res.render("home");
 });
 
+app.get("/campgrounds", (req, res) => {
+    var campgrounds = [
+        {name: "Algonquin National Park", image: "https://www.hinzie.com/media/image/60103_max.jpg"},
+        {name: "Killbear Provincial Park", image: "https://i0.wp.com/rvplacestogo.com/wp-content/uploads/2017/07/feature-1.jpg"},
+        {name: "Letchworth State Park", image: "https://www.travelingmom.com/wp-content/uploads/2015/10/DSC04764_edited-1-800x450.jpg"}
+    ];
+
+    res.render("campgrounds", {campgrounds: campgrounds});
+});
+
 // Tell Express to listen for requests (start server).
 app.listen(port, () => {
     console.log("The YelpCamp server is running, listening on port " + port);
