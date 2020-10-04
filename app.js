578264d177482309dfc44075079c7b603586a48f
Tyler Kalosza <tyler.kalosza@gmail.com>
2020-10-04 13:25:31 -0400

    Body-parser is set up.

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const port = process.env.PORT || 1337;
 const express = require("express");
+const bodyParser = require("body-parser");
+
 const app = express();
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 
