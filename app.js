05d7c9f27650888446eba6f8cf2ee67c00a820d1
Tyler Kalosza <tyler.kalosza@gmail.com>
2020-10-04 16:20:19 -0400

    Added campgroundSchema and renamed imageUrl to image.

--- app.js
+++ app.js
@@ -9,16 +9,38 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 
-app.get("/", (req, res) => {
-    res.render("home");
-});
+mongoose.connect("mongodb://localhost/yelp-camp", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => console.log("Connected to database!"))
+.catch(error => console.log(error.message));
 
 // This is temporary.
 var campgrounds = [
-    {name: "Algonquin National Park", imageUrl: "https://www.hinzie.com/media/image/60103_max.jpg"},
-    {name: "Killbear Provincial Park", imageUrl: "https://i0.wp.com/rvplacestogo.com/wp-content/uploads/2017/07/feature-1.jpg"},
-    {name: "Letchworth State Park", imageUrl: "https://www.travelingmom.com/wp-content/uploads/2015/10/DSC04764_edited-1-800x450.jpg"}
+    {name: "Algonquin National Park", image: "https://www.hinzie.com/media/image/60103_max.jpg"},
+    {name: "Killbear Provincial Park", image: "https://i0.wp.com/rvplacestogo.com/wp-content/uploads/2017/07/feature-1.jpg"},
+    {name: "Letchworth State Park", image: "https://www.travelingmom.com/wp-content/uploads/2015/10/DSC04764_edited-1-800x450.jpg"}
 ];
+
+//--------
+// Schema
+//--------
+
+var campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String
+});
+
+var Campground = mongoose.model("Campground", campgroundSchema);
+
+//--------
+// Routes
+//--------
+
+app.get("/", (req, res) => {
+    res.render("home");
+});
 
 app.get("/campgrounds", (req, res) => {
     res.render("campgrounds", {campgrounds: campgrounds});
@@ -26,9 +48,9 @@ app.get("/campgrounds", (req, res) => {
 
 app.post("/campgrounds", (req, res) => {
     var name = req.body.name;
-    var imageUrl = req.body.imageUrl;
+    var image = req.body.image;
 
-    campgrounds.push({name: name, imageUrl: imageUrl});
+    campgrounds.push({name: name, image: image});
     res.redirect("/campgrounds");
 });
 
