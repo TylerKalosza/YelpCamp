efde17c46ae60c23c1bd658728fb1925297b28f5
Tyler Kalosza <tyler.kalosza@gmail.com>
2020-10-05 23:13:30 -0400

    Added some styling to the show campground page.

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const Comment = require("./models/comment");
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static("public"));
+app.use(express.static(__dirname + "/public"));
 app.set("view engine", "ejs");
 
 mongoose.connect("mongodb://localhost:27017/yelp-camp", {
