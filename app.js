7ce85c35bb603912c0a4277f13dc4dd36735c749
Tyler Kalosza <tyler.kalosza@gmail.com>
2020-10-07 11:15:15 -0400

    Connected connect-flash npm package.

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
+const flash = require("connect-flash");
 const methodOverride = require("method-override");
 const seedDb = require("./seeds")
 
@@ -22,6 +23,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + "/public"));
 app.use(methodOverride("_method"));
 app.set("view engine", "ejs");
+app.use(flash());
 
 mongoose.connect("mongodb://localhost:27017/yelp-camp", {
     useNewUrlParser: true,
